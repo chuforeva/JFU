ac5c6bb6148212ee4fcf026a556ca3bdb1187cd3
Sejung Hwang <hsj9886@vt.edu>
2025-01-13 15:23:52 -0500

    pages added

--- 2025/scripts/import.js
+++ 2025/scripts/import.js
@@ -32,7 +32,7 @@ function insertNav(loc) {
     nav.appendChild(getNavBtn(loc, "투두리스트", '/todo'));
     nav.appendChild(getNavBtn(loc, "원클릭 스밍리스트", '/oneclick'));
     nav.appendChild(getNavBtn(loc, "가이드", '/guide'));
-    nav.appendChild(getNavBtn(loc, "음악방송 투표", '/vote'));
+    nav.appendChild(getNavBtn(loc, "투표", '/vote'));
     nav.appendChild(getNavBtn(loc, '앨범 공동구매 <i class="fa-solid fa-up-right-from-square"></i>', 'https://kr.ktown4u.com/eventsub?eve_no=6512415&biz_no=967'));
 }
 
@@ -56,7 +56,7 @@ function getNavBtn(loc, text, url) {
 function insertMobileNav(loc) {
     var nav = document.createElement('div');
     nav.id = "mobile_nav";
-    
+
     nav.appendChild(getNavBtn(loc, `<i class="fa-solid fa-house"></i><span>메인</span>`, '/'));
     nav.appendChild(getNavBtn(loc, `<i class="fa-solid fa-list"></i><span>투두리스트</span>`, '/todo'));
     nav.appendChild(getNavBtn(loc, `<i class="fa-solid fa-hand-pointer"></i><span>원클릭</span>`, '/oneclick'));
@@ -69,6 +69,35 @@ function insertMobileNav(loc) {
 function insertFooter() {
     var footer = document.getElementsByTagName('footer')[0];
     var img = document.createElement('img');
-    img.src = `${origin}/images/logos/JFU_footer.png`;
+    img.src = `${origin}/images/logos/JFU_logo_5.png`;
+    var span = document.createElement('span');
+    span.innerHTML = "&copy; 2023-2025 JISOO FAN UNION | ALL FOR BLACKPINK JISOO<br>WARNING: ALL RIGHTS RESERVED | UNAUTHORIZED COMMERCIAL USE OR PUBLICATION IS PROHIBITED";
     footer.appendChild(img);
+    footer.appendChild(span);
+}
+
+function popup() {
+    var popup = document.createElement('div');
+    popup.id = "popup";
+    var popup_header = document.createElement('div');
+    popup_header.id = "popup_header";
+    popup_header.innerText = "JISOO 2nd SOLO SUPPORT";
+    var popup_close = document.createElement('span');
+    popup_close.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+    popup_close.addEventListener("click", function(e) {
+        document.getElementById('popup').setAttribute('style', 'display: none');
+    });
+
+    popup_header.appendChild(popup_close);
+    popup.appendChild(popup_header);
+    var popup_content = document.createElement('div');
+    popup_content.id = "popup_content";
+    popup_content.innerHTML += `<iframe src="https://www.youtube.com/embed/7JF8GpBjXH4" title="JISOO COMING SOON" id="youtube_embed" frameborder="0" allowfullscreen style="width: 100%;"></iframe>
+        카카오뱅크 ㅂㄷㄱ<br>
+        <span style="font-size: 2rem;">3333 17 3766359</span><br><br>
+        DC 지수 갤러리에서 다가오는 지수의 두 번째 솔로 활동 서포트를 위한 모금을 진행합니다!<br>많은 후원 부탁드립니다.<br>
+        모금 받은 금액은 추후 지수팬연합 계좌로 전액 이체되며, 오로지 지수의 솔로 활동 서포트에 사용됩니다.<br>
+        정산은 서포트가 최종적으로 마무리 된 후 입금자에 한해 공개 예정입니다.`;
+    popup.appendChild(popup_content);
+    document.getElementsByTagName('footer')[0].after(popup);
 }
